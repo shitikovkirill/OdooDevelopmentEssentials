8e974b02315fb5b66c3bce9836db57c6c44b32b6
shitikovkirill <sh.kiruh@gmail.com>
2017-06-03 08:05:47 +0300

    Modifying existing fields

--- todo_task.py
+++ todo_task.py
@@ -4,5 +4,6 @@ from openerp import models, fields, api
 
 class TodoTask(models.Model):
     _inherit = 'todo.task'
+    name = fields.Char(help="What needs to be done?")
     user_id = fields.Many2one('res.users', 'Responsible')
     date_deadline = fields.Date('Deadline')
