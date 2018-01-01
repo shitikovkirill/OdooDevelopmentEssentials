174396a8ce79368984900e64e42914cdf78dfeb3
Shitikov Kyrylo <kshy@isddesign.com>
2018-01-01 16:13:20 +0200

    Create new module ToDo-wizard

---
+++ todo_wizard/__openerp__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'To-Do Tasks Management Assistant',
+    'description': 'Mass edit your To-Do backlog.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_user'],
+    'data': ['todo_wizard_view.xml'],
+}
