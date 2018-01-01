dbd435a3e6c2ffc976f52d7bea8d1d3a3d862996
Shitikov Kyrylo <kshy@isddesign.com>
2018-01-01 21:29:00 +0200

    Add mail mail_thread to project.

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Multiuser To-Do',
     'description': 'Extend the To-Do app to multiuser.',
     'author': 'Daniel Reis',
-    'depends': ['todo_app'],
+    'depends': ['todo_app', 'mail'],
     'data': [
         'todo_view.xml',
         'security/todo_access_rules.xml',
